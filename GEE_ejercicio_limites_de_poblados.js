35bb82351b8b261d909e1530340fc979ff9dc8d7
PhD. Abel Argueta <abel.argueta@ues.edu.sv>
2025-06-02 15:26:06 -0600

    Update GEE_ejercicio_limites_de_poblados.js
    Autoría

--- GEE_ejercicio_limites_de_poblados.js
+++ GEE_ejercicio_limites_de_poblados.js
@@ -1,3 +1,6 @@
+//Script elaborado por Abner Jiménez - Coordinador Técnico Regional
+//Coordinador Técnico Regional - Proyecto Paisajes Forestales y Comercio Sostenible REDD+Landscape III
+//GIZ - Deutsche Gesellschaft für Internationale Zusammenarbeit GmbH
 // =====================================================================================
 // Script para generar límites de poblados (método RASTER)
 // =====================================================================================
